c88a90f7647130f0c52bd759a3272c9136a3ec57
ggsavin <georgesavin@cmail.carleton.ca>
2023-06-30 13:34:18 -0400

    minor cleanup of files

--- test-mlflow.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-mlflow.set_tracking_uri("http://mlflow:5000")
-
-with mlflow.start_run(run_name='Wolf Experiment'):
-    mlflow.log_param("t1", 10)
-    mlflow.log_metric("m1", 10)
-    mlflow.log_metric("m1", 20)
